5c7e78a1f00e489a2af220cc5b9cc05124b263d5
‘Achin001’ <646897706@qq.com>
2021-06-21 14:52:04 +0800

    “判断了重复添加购物车接口”

--- src/main/java/com/gxc/sldz/controller/API/SldzShopCartApi.java
+++ src/main/java/com/gxc/sldz/controller/API/SldzShopCartApi.java
@@ -2,13 +2,11 @@ package com.gxc.sldz.controller.API;
 
 
 import cn.hutool.core.util.ObjectUtil;
-import cn.hutool.crypto.SecureUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.diboot.core.vo.JsonResult;
 import com.diboot.core.vo.Pagination;
 import com.gxc.sldz.controller.BaseCustomCrudRestController;
 import com.gxc.sldz.dto.SldzShopCartDTO;
-import com.gxc.sldz.entity.SldzAdmin;
 import com.gxc.sldz.entity.SldzShopCart;
 import com.gxc.sldz.service.SldzShopCartService;
 import com.gxc.sldz.vo.SldzShopCartListVO;
@@ -19,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Api(tags = {"购物车前台接口"})
 @RestController
@@ -73,7 +72,10 @@ public class SldzShopCartApi extends BaseCustomCrudRestController<SldzShopCart> 
         if (ObjectUtil.isNotNull(SldzShopCart)){
             //有则添加数量
             SldzShopCart.setCartNum(SldzShopCart.getCartNum()+1);
-            sldzShopCartService.updateEntity(SldzShopCart);
+            if (sldzShopCartService.updateEntity(SldzShopCart)){
+                return JsonResult.OK();
+            }
+            return JsonResult.FAIL_OPERATION("添加失败");
         }
         //无则新建购物车记录
         return super.createEntity(entity);
@@ -122,16 +124,30 @@ public class SldzShopCartApi extends BaseCustomCrudRestController<SldzShopCart> 
     }
 
 
-    /***
-     * 根据id删除资源对象
-     * @param id
-     * @return
-     * @throws Exception
-     */
+//    /***
+//     * 根据id删除资源对象
+//     * @param id
+//     * @return
+//     * @throws Exception
+//     */
     @ApiOperation(value = "根据ID删除数据")
     @DeleteMapping("/{id}")
     public JsonResult deleteEntityMapping(@PathVariable("id")Long id) throws Exception {
         return super.deleteEntity(id);
     }
+
+    /***
+     * 根据id删除资源对象
+     * @param
+     * @return
+     * @throws Exception
+     */
+//    @ApiOperation(value = "根据IDS删除数据")
+//    @PostMapping("/deleteByIds")
+//    public boolean deleteEntityMapping(@RequestBody List<String> listids) throws Exception {
+//        System.out.println(listids);
+//        return  true;
+//    }
+
 
 }
