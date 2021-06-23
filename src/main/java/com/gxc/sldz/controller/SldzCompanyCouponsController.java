318c79259e99a869f7cd7d7f5eda3e32bc0bd3ab
‘Achin001’ <646897706@qq.com>
2021-06-23 13:55:35 +0800

    “添加了订单接口”

--- src/main/java/com/gxc/sldz/controller/SldzCompanyCouponsController.java
+++ src/main/java/com/gxc/sldz/controller/SldzCompanyCouponsController.java
@@ -4,93 +4,91 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-
 import com.diboot.core.vo.*;
 import com.gxc.sldz.entity.SldzCompanyCoupons;
 import com.gxc.sldz.dto.SldzCompanyCouponsDTO;
 import com.gxc.sldz.vo.SldzCompanyCouponsListVO;
 import com.gxc.sldz.vo.SldzCompanyCouponsDetailVO;
 import com.gxc.sldz.service.SldzCompanyCouponsService;
-
 import lombok.extern.slf4j.Slf4j;
 import javax.validation.Valid;
 import java.util.List;
 
 /**
-* 公司优惠券 相关Controller
-* @author Achin
-* @version 1.0
-* @date 2021-06-22
-* Copyright © MyCompany
-*/
-@Api(tags = {"公司优惠券后台管理"})
+ * 公司优惠券 相关Controller
+ * @author Achin
+ * @version 1.0
+ * @date 2021-06-22
+ * Copyright © MyCompany
+ */
+@Api(tags = { "公司优惠券后台管理" })
 @RestController
 @RequestMapping("admin/sldzCompanyCoupons")
 @Slf4j
 public class SldzCompanyCouponsController extends BaseCustomCrudRestController<SldzCompanyCoupons> {
+
     @Autowired
     private SldzCompanyCouponsService sldzCompanyCouponsService;
 
-    /***
-    * 查询ViewObject的分页数据
-    * <p>
-    * url请求参数示例: /list?field=abc&pageIndex=1&orderBy=abc:DESC
-    * </p>
-    * @return
-    * @throws Exception
-    */
+    /**
+     * 查询ViewObject的分页数据
+     * <p>
+     * url请求参数示例: /list?field=abc&pageIndex=1&orderBy=abc:DESC
+     * </p>
+     * @return
+     * @throws Exception
+     */
     @ApiOperation(value = "获取列表分页数据")
     @GetMapping("/list")
-    public JsonResult getViewObjectListMapping(SldzCompanyCouponsDTO queryDto, Pagination pagination) throws Exception{
+    public JsonResult getViewObjectListMapping(SldzCompanyCouponsDTO queryDto, Pagination pagination) throws Exception {
         return super.getViewObjectList(queryDto, pagination, SldzCompanyCouponsListVO.class);
     }
 
-    /***
-    * 根据资源id查询ViewObject
-    * @param id ID
-    * @return
-    * @throws Exception
-    */
+    /**
+     * 根据资源id查询ViewObject
+     * @param id ID
+     * @return
+     * @throws Exception
+     */
     @ApiOperation(value = "根据ID获取详情数据")
     @GetMapping("/{id}")
-    public JsonResult getViewObjectMapping(@PathVariable("id")Long id) throws Exception{
+    public JsonResult getViewObjectMapping(@PathVariable("id") Long id) throws Exception {
         return super.getViewObject(id, SldzCompanyCouponsDetailVO.class);
     }
 
-    /***
-    * 创建资源对象
-    * @param entity
-    * @return JsonResult
-    * @throws Exception
-    */
+    /**
+     * 创建资源对象
+     * @param entity
+     * @return JsonResult
+     * @throws Exception
+     */
     @ApiOperation(value = "新建数据")
     @PostMapping("/")
     public JsonResult createEntityMapping(@Valid @RequestBody SldzCompanyCoupons entity) throws Exception {
         return super.createEntity(entity);
     }
 
-    /***
-    * 根据ID更新资源对象
-    * @param entity
-    * @return JsonResult
-    * @throws Exception
-    */
+    /**
+     * 根据ID更新资源对象
+     * @param entity
+     * @return JsonResult
+     * @throws Exception
+     */
     @ApiOperation(value = "根据ID更新数据")
     @PutMapping("/{id}")
-    public JsonResult updateEntityMapping(@PathVariable("id")Long id, @Valid @RequestBody SldzCompanyCoupons entity) throws Exception {
+    public JsonResult updateEntityMapping(@PathVariable("id") Long id, @Valid @RequestBody SldzCompanyCoupons entity) throws Exception {
         return super.updateEntity(id, entity);
     }
 
-    /***
-    * 根据id删除资源对象
-    * @param id
-    * @return
-    * @throws Exception
-    */
+    /**
+     * 根据id删除资源对象
+     * @param id
+     * @return
+     * @throws Exception
+     */
     @ApiOperation(value = "根据ID删除数据")
     @DeleteMapping("/{id}")
-    public JsonResult deleteEntityMapping(@PathVariable("id")Long id) throws Exception {
+    public JsonResult deleteEntityMapping(@PathVariable("id") Long id) throws Exception {
         return super.deleteEntity(id);
     }
-
-} 
+}
--- src/main/java/com/gxc/sldz/entity/SldzCompanyCoupons.java
+++ src/main/java/com/gxc/sldz/entity/SldzCompanyCoupons.java
@@ -4,7 +4,6 @@ import java.util.Date;
 import java.lang.Double;
 import java.math.BigDecimal;
 import javax.validation.constraints.NotNull;
-
 import com.fasterxml.jackson.annotation.JsonFormat;
 import org.hibernate.validator.constraints.Length;
 import java.util.List;
@@ -23,76 +22,85 @@ import lombok.Setter;
 import lombok.experimental.Accessors;
 
 /**
-* 公司优惠券 Entity定义
-* @author Achin
-* @version 1.0
-* @date 2021-06-22
-* Copyright © MyCompany
-*/
+ * 公司优惠券 Entity定义
+ * @author Achin
+ * @version 1.0
+ * @date 2021-06-22
+ * Copyright © MyCompany
+ */
 @ApiModel(value = "公司优惠券")
-@Getter @Setter @Accessors(chain = true)
+@Getter
+@Setter
+@Accessors(chain = true)
 public class SldzCompanyCoupons extends BaseCustomEntity {
+
     private static final long serialVersionUID = 4261816699039245477L;
 
     /**
-    * 吱口令 
-    */
-    @ApiModelProperty(value="吱口令", example = "")
-    @Length(max=100, message="吱口令长度应小于100")
+     * 优惠券名称
+     */
+    @ApiModelProperty(value = "优惠券名称", example = "")
+    @Length(max = 100, message = "优惠券名称长度应小于100")
+    @TableField()
+    private String couponsName;
+
+    /**
+     * 吱口令
+     */
+    @ApiModelProperty(value = "吱口令", example = "")
+    @Length(max = 100, message = "吱口令长度应小于100")
     @TableField()
     private String couponsSpwd;
 
     /**
-    * 优惠券大厅是否显示(1显示,2不显示) 
-    */
-    @ApiModelProperty(value="优惠券大厅是否显示(1显示,2不显示)", example = "true")
+     * 优惠券大厅是否显示(1显示,2不显示)
+     */
+    @ApiModelProperty(value = "优惠券大厅是否显示(1显示,2不显示)", example = "true")
     @TableField()
     private Boolean couponsShowOrHide = true;
 
     /**
-    * 优惠券总数量 
-    */
-    @ApiModelProperty(value="优惠券总数量", example = "")
+     * 优惠券总数量
+     */
+    @ApiModelProperty(value = "优惠券总数量", example = "")
     @TableField()
     private Integer couponsTotal;
 
     /**
-    * 优惠券失效时间 
-    */
-    @ApiModelProperty(value="优惠券失效时间", example = "2021-06-22 00:00")
+     * 优惠券失效时间
+     */
+    @ApiModelProperty(value = "优惠券失效时间", example = "2021-06-22 00:00")
     @JsonFormat(pattern = D.FORMAT_DATETIME_Y4MDHM)
     @TableField()
     private Date couponsFailureTime;
 
     /**
-    * 满多少 
-    */
-    @ApiModelProperty(value="满多少", example = "")
+     * 满多少
+     */
+    @ApiModelProperty(value = "满多少", example = "")
     @TableField()
     private Double couponsFullPrice;
 
     /**
-    * 减多少 
-    */
-    @ApiModelProperty(value="减多少", example = "")
+     * 减多少
+     */
+    @ApiModelProperty(value = "减多少", example = "")
     @TableField()
     private Double couponsReducePrice;
 
     /**
-    * 指定可用产品id(  数组 ) 
-    */
-    @ApiModelProperty(value="指定可用产品id(  数组 )", example = "")
-    @Length(max=500, message="指定可用产品id(  数组 )长度应小于500")
+     * 指定可用产品id(  数组 )
+     */
+    @ApiModelProperty(value = "指定可用产品id(  数组 )", example = "")
+    @Length(max = 500, message = "指定可用产品id(  数组 )长度应小于500")
     @TableField()
     private String couponsAppointProductIds;
 
     /**
-    * 详情 
-    */
-    @ApiModelProperty(value="详情", example = "")
-    @Length(max=500, message="详情长度应小于500")
+     * 详情
+     */
+    @ApiModelProperty(value = "详情", example = "")
+    @Length(max = 500, message = "详情长度应小于500")
     @TableField()
     private String couponsDetails;
-
-
-} 
+}
