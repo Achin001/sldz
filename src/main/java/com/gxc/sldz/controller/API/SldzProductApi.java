5ab5d5adfee7f2364aba70437bd598ec7930aa89
‘Achin001’ <646897706@qq.com>
2021-07-12 15:15:05 +0800

    “修复了退款证书问题”

--- src/main/java/com/gxc/sldz/controller/API/SldzProductApi.java
+++ src/main/java/com/gxc/sldz/controller/API/SldzProductApi.java
@@ -5,11 +5,13 @@ import com.diboot.core.vo.Pagination;
 import com.gxc.sldz.controller.BaseCustomCrudRestController;
 import com.gxc.sldz.dto.SldzProductDTO;
 import com.gxc.sldz.entity.SldzProduct;
+import com.gxc.sldz.vo.SldzOrderDetailVO;
 import com.gxc.sldz.vo.SldzProductListVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,6 +35,18 @@ public class SldzProductApi  extends BaseCustomCrudRestController<SldzProduct> {
     @GetMapping("/list")
     public JsonResult getViewObjectListMapping(SldzProductDTO queryDto, Pagination pagination) throws Exception {
         return super.getViewObjectList(queryDto, pagination, SldzProductListVO.class);
+    }
+
+    /***
+     * 根据资源id查询ViewObject
+     * @param id ID
+     * @return
+     * @throws Exception
+     */
+    @ApiOperation(value = "根据ID获取详情数据")
+    @GetMapping("/{id}")
+    public JsonResult getViewObjectMapping(@PathVariable("id") Long id) throws Exception {
+        return super.getViewObject(id, SldzOrderDetailVO.class);
     }
 
 
